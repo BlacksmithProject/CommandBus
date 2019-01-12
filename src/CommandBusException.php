a47161219f2152b624a240860ae152ec5d97ea24
Nicolas Gomes de Oliveira <ngdo27@gmail.com>
2019-01-12 01:16:55 +0100

    feat(CommandBus): now properly throw exception when command is not declared

---
+++ src/CommandBusException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace BSP;
+
+final class CommandBusException extends \Exception
+{
+}
--- tests/CommandBusTest.php
+++ tests/CommandBusTest.php
@@ -3,14 +3,19 @@ declare(strict_types=1);
 
 namespace BSP\Tests;
 
+use BSP\CommandBusException;
 use BSP\Tests\Mock\AddSugarToCoffee;
 use BSP\Tests\Mock\AddSugarToCoffeHandler;
 use BSP\Tests\Mock\Coffee;
 use BSP\Tests\Mock\CoffeeCommandBus;
+use BSP\Tests\Mock\ServeCoffeeInCup;
 use PHPUnit\Framework\TestCase;
 
 final class CommandBusTest extends TestCase
 {
+    /**
+     * @var CoffeeCommandBus
+     */
     private $commandBus;
 
     public function setUp()
@@ -19,6 +24,9 @@ final class CommandBusTest extends TestCase
         $this->commandBus = new CoffeeCommandBus($commandHandler);
     }
 
+    /**
+     * @throws CommandBusException
+     */
     public function testCommandBus(): void
     {
         $coffee = new Coffee();
@@ -30,5 +38,18 @@ final class CommandBusTest extends TestCase
         $this->commandBus->execute($command);
 
         $this->assertSame(2, $coffee::$sugars);
+    }
+
+    /**
+     * @throws CommandBusException
+     */
+    public function testCannotUseCommandBusWithoutHandler(): void
+    {
+        $this->expectException(CommandBusException::class);
+        $this->expectExceptionMessage('This Commandbus cannot handle command "BSP\Tests\Mock\ServeInCup".');
+
+        $command = new ServeCoffeeInCup();
+
+        $this->commandBus->execute($command);
     }
 }
